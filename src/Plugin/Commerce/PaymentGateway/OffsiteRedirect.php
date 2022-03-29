50aafc9f216ba33378a149e05904185396c23fcb
Roman K <karbivskij@gmail.com>
2022-03-29 07:19:53 +0300

    ready

---
+++ src/Plugin/Commerce/PaymentGateway/OffsiteRedirect.php
@@ -0,0 +1,246 @@
+<?php
+
+  namespace Drupal\commerce_viva\Plugin\Commerce\PaymentGateway;
+
+  use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
+  use Drupal\Core\Form\FormStateInterface;
+  use Drupal\commerce_viva\PluginForm\OffsiteRedirect\FondyOffsiteForm;
+  use Drupal\Core\Language\LanguageInterface;
+  use Drupal\commerce_order\Entity\OrderInterface;
+  use Symfony\Component\HttpFoundation\Request;
+  use Drupal\commerce_order\Entity\Order;
+
+  /**
+   * Provides the Viva Wallet offsite Checkout payment gateway.
+   *
+   * @CommercePaymentGateway(
+   *   id = "viva_redirect",
+   *   label = @Translation("VivaWallet (Redirect to payment page)"),
+   *   display_label = @Translation("Viva Wallet"),
+   *    forms = {
+   *     "offsite-payment" = "Drupal\commerce_viva\PluginForm\OffsiteRedirect\FondyOffsiteForm",
+   *   },
+   * )
+   */
+  class OffsiteRedirect extends OffsitePaymentGatewayBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration() {
+      return [
+        'merchant_id' => '',
+        'api_key' => '',
+        'website_code' => '',
+        'client_id' => '',
+        'client_secret' => '',
+      ] + parent::defaultConfiguration();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form,
+                                           FormStateInterface $form_state) {
+      $form = parent::buildConfigurationForm($form, $form_state);
+
+      $form['merchant_id'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Merchant ID'),
+        '#description' => $this->t('This is the Merchant ID from the Viva Wallet.'),
+        '#default_value' => $this->configuration['merchant_id'],
+        '#required' => TRUE,
+      ];
+
+      $form['api_key'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('API key'),
+        '#description' => $this->t('This is the API key from the Viva Wallet.'),
+        '#default_value' => $this->configuration['api_key'],
+        '#required' => TRUE,
+      ];
+
+      $form['website_code'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Website code'),
+        '#description' => $this->t('This is the website code (4 digit) from the Viva Wallet.'),
+        '#default_value' => $this->configuration['website_code'],
+        '#required' => TRUE,
+      ];
+
+      $form['client_id'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Client ID'),
+        '#description' => $this->t('This is the Client ID from the Viva Wallet(Smart Checkout Credentials).'),
+        '#default_value' => $this->configuration['client_id'],
+        '#required' => TRUE,
+      ];
+
+      $form['client_secret'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Client Secret'),
+        '#description' => $this->t('This is the Client ID from the Viva Wallet (Smart Checkout Credentials).'),
+        '#default_value' => $this->configuration['client_secret'],
+        '#required' => TRUE,
+      ];
+
+      return $form;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+      parent::submitConfigurationForm($form, $form_state);
+      if (!$form_state->getErrors()) {
+        $values = $form_state->getValue($form['#parents']);
+        $this->configuration['merchant_id'] = $values['merchant_id'];
+        $this->configuration['api_key'] = $values['api_key'];
+        $this->configuration['website_code'] = $values['website_code'];
+        $this->configuration['client_id'] = $values['client_id'];
+        $this->configuration['client_secret'] = $values['client_secret'];
+      }
+    }
+
+    /**
+     * @param OrderInterface $order
+     * @param Request $request
+     * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+     * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+     * @throws \Drupal\Core\Entity\EntityStorageException
+     */
+    public function onReturn(OrderInterface $order, Request $request) {
+
+      $settings = [
+        'api_key' => $this->configuration['api_key'],
+        'merchant_id' => $this->configuration['merchant_id'],
+        'website_code' => $this->configuration['website_code'],
+        'client_id' => $this->configuration['client_id'],
+        'client_secret' => $this->configuration['client_secret']
+      ];
+      $data = $request->request->all();
+      list($orderId,) = explode(FondyOffsiteForm::ORDER_SEPARATOR, $data['order_id']);
+      if ($this->isPaymentValid($settings, $data, $order) !== TRUE) {
+        $this->messenger()->addMessage($this->t('Invalid Transaction. Please try again'), 'error');
+        return $this->onCancel($order, $request);
+      }
+      else {
+        $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+        $payment = $payment_storage->create([
+          'state' => 'completed',
+          'amount' => $order->getTotalPrice(),
+          'payment_gateway' => $this->entityId,
+          'order_id' => $orderId,
+          'remote_id' => $data['payment_id'],
+          'remote_state' => $data['order_status'],
+          'sourceCode' => '3573',
+        ]);
+        $payment->save();
+        $this->messenger()->addMessage(
+          $this->t('Your payment was successful with Order id : @orderid and Transaction id : @payment_id',
+            [
+              '@orderid' => $order->id(),
+              '@payment_id' => $data['payment_id']
+            ]
+          ));
+      }
+    }
+
+    /**
+     * @param Request $request
+     * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+     * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+     * @throws \Drupal\Core\Entity\EntityStorageException
+     */
+    public function onNotify(Request $request) {
+      $settings = [
+        'api_key' => $this->configuration['api_key'],
+        'merchant_id' => $this->configuration['merchant_id'],
+        'website_code' => $this->configuration['website_code'],
+        'client_id' => $this->configuration['client_id'],
+        'client_secret' => $this->configuration['client_secret']
+      ];
+      $data = $request->request->all();
+      if (!$data)
+        $data = $request->getContent();
+      list($orderId,) = explode(FondyOffsiteForm::ORDER_SEPARATOR, $data['order_id']);
+      $order = Order::load($orderId);
+      if ($this->isPaymentValid($settings, $data, $order) !== TRUE) {
+        die($this->t('Invalid Transaction. Please try again'));
+      }
+      else {
+        $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+        if ($data['order_status'] == 'expired' or $data['order_status'] == 'declined') {
+          $order->set('state', 'cancelled');
+          $order->save();
+        }
+        $last = $payment_storage->loadByProperties([
+          'payment_gateway' => $this->entityId,
+          'order_id' => $orderId,
+          'remote_id' => $data['payment_id']
+        ]);
+        if (!empty($last)) {
+          $payment_storage->delete($last);
+        }
+        $payment = $payment_storage->create([
+          'state' => 'completed',
+          'amount' => $order->getTotalPrice(),
+          'payment_gateway' => $this->entityId,
+          'order_id' => $orderId,
+          'remote_id' => $data['payment_id'],
+          'remote_state' => $data['order_status'],
+          'sourceCode' => '3573',
+        ]);
+        $payment->save();
+        die('Ok');
+      }
+    }
+
+    /**
+     * @param $settings
+     * @param $response
+     *
+     * @return bool
+     */
+    public function isPaymentValid($settings, $response, $order) {
+      if (!$response) {
+        return FALSE;
+      }
+      if ($settings['merchant_id'] != $response['merchant_id']) {
+        return FALSE;
+      }
+      $transaction_currency = $response['currency'];
+      $transaction_amount = $response['amount'] / 100;
+      $order_currency = $order->getTotalPrice()->getCurrencyCode();
+      $order_amount = $order->getTotalPrice()->getNumber();
+
+      if (!$this->validateSum($transaction_currency, $order_currency,
+        $transaction_amount, $order_amount)
+      ) {
+        return FALSE;
+      }
+
+      return TRUE;
+    }
+
+    /**
+     * @param $transaction_currency
+     * @param $order_currency
+     * @param $transaction_amount
+     * @param $order_amount
+     *
+     * @return bool
+     */
+    protected function validateSum($transaction_currency, $order_currency,
+                                   $transaction_amount, $order_amount) {
+      if ($transaction_currency != $order_currency) {
+        return FALSE;
+      }
+      if ($transaction_amount != $order_amount) {
+        return FALSE;
+      }
+
+      return TRUE;
+    }
+
+  }
---
+++ src/PluginForm/OffsiteRedirect/FondyOffsiteForm.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Drupal\commerce_viva\PluginForm\OffsiteRedirect;
+
+use Drupal\commerce_order\Entity\Order;
+use Drupal\commerce_payment\PluginForm\PaymentOffsiteForm as BasePaymentOffsiteForm;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\commerce_viva\PluginForm\OffsiteRedirect;
+
+class FondyOffsiteForm extends BasePaymentOffsiteForm
+{
+
+  public function order_code(){
+      //Viva
+      $payment = $this->entity;
+      $amount = round(number_format($payment->getAmount()
+            ->getNumber(), 2, '.', '') * 100);
+      $order_id = \Drupal::routeMatch()->getParameter('commerce_order')->id();
+      $order = Order::load($order_id);
+      $address = $order->getBillingProfile()->address->first();
+      if ($payment->getOrder()->getCustomer()->isAnonymous() === FALSE) {
+        $description = t('Customer: ') . $payment->getOrder()
+            ->getCustomer()
+            ->getAccountName() . '. ' . t('Order #: ') . $order_id;
+      } else {
+        $description = t('Customer: anonymous');
+      }
+
+      $curl = curl_init();
+
+      $payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
+      $configuration = $payment_gateway_plugin->getConfiguration();
+
+      $mid = $configuration['merchant_id'];
+      $api_key = $configuration['api_key'];
+      $website_code = $configuration['website_code'];
+
+      $b64str = $mid.':'.$api_key;
+      $b64str_encode = base64_encode($b64str);
+
+      $order_info = json_encode([
+        'amount' => $amount,
+        'email' => $order->getEmail(),
+        'fullName' => $address->getGivenName(),
+        'customerTrns' => $description,
+        'requestLang' => 'en-GB'
+      ]);
+     /* $order_info = '{
+        "amount": 100,
+        "email": "customer@domain.com",
+        "fullName": "Customer name",
+        "customerTrns": "Short description of items/services purchased to display to your customer",
+        "requestLang": "en-GB"
+      }';*/
+      
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://demo.vivapayments.com/api/orders',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => $order_info,
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json',
+      'Authorization: Basic '.$b64str_encode
+    ),
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  $response = json_decode($response, true);
+  $code_url = $response['OrderCode'];
+  $order_code_url = 'https://demo.vivapayments.com/web/checkout?ref='.$code_url;
+  return $order_code_url;
+//Viva
+  }
+
+  /**
+   * @var string pay url
+   */
+  //$url = FondyOffsiteForm::order_code();
+  
+  /**
+   * additional cons
+   */
+  const ORDER_SEPARATOR = '#';
+  const SIGNATURE_SEPARATOR = '|';
+
+  /**
+   * @param array $form
+   * @param FormStateInterface $form_state
+   * @return array
+   * @throws \Drupal\commerce\Response\NeedsRedirectException
+   */
+
+   //Global variables
+
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+  {
+    
+    $payment = $this->entity;
+    $amount = round(number_format($payment->getAmount()
+          ->getNumber(), 2, '.', '') * 100);
+    $order_id = \Drupal::routeMatch()->getParameter('commerce_order')->id();
+    $order = Order::load($order_id);
+    $address = $order->getBillingProfile()->address->first();
+
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
+
+    $configuration = $payment_gateway_plugin->getConfiguration();
+
+    $redirect_method = 'post';
+    $redirect_url = $this->order_code();
+    $mid = $configuration['merchant_id'];
+    
+    $currency_code = $payment->getAmount()->getCurrencyCode();
+    if ($payment->getOrder()->getCustomer()->isAnonymous() === FALSE) {
+      $description = t('Customer: ') . $payment->getOrder()
+          ->getCustomer()
+          ->getAccountName() . '. ' . t('Order #: ') . $order_id;
+      $subscriber_id = $payment->getOrder()->getCustomerId();
+    } else {
+      $description = t('Customer: anonymous');
+      $subscriber_id = '';
+    }
+    $callbackurl = $payment_gateway_plugin->getNotifyUrl()->toString();
+    $responseurl = Url::FromRoute('commerce_payment.checkout.return', [
+      'commerce_order' => $order_id,
+      'step' => 'payment'
+    ], ['absolute' => TRUE])->toString();
+
+    $order = Order::load($order_id);
+    $address = $order->getBillingProfile()->address->first();
+    // Get the language of credit card form.
+    $language = $configuration['language'];
+    if ($language == LanguageInterface::LANGCODE_NOT_SPECIFIED &&
+      $customer = $order->getCustomer()
+    ) {
+      // Use account preferred language.
+      $language = $customer->getPreferredLangcode();
+    }
+    $f_data = [
+      'merchant_id' => $mid,
+      'order_id' => $order_id . self::ORDER_SEPARATOR . time(),
+      'order_desc' => $description,
+      'amount' => $amount,
+      'merchant_data' => json_encode([
+        'subscriber_id' => $subscriber_id,
+        'custom_field_customer_name' => $address->getGivenName(),
+        'custom_field_customer_address' => $address->getAddressLine1(),
+        'custom_field_customer_city' => $address->getLocality(),
+        'custom_field_customer_country' => $address->getAdministrativeArea(),
+        'custom_field_customer_state' => $address->getCountryCode(),
+        'custom_field_customer_zip' => $address->getPostalCode(),
+        'custom_field_sender_email' => $order->getEmail()
+      ]),
+      'currency' => $currency_code,
+      'response_url' => $responseurl,
+      'server_callback_url' => $callbackurl,
+      'sender_email' => $order->getEmail(),
+      'lang' => strtolower($language)
+    ];
+
+    $f_data['signature'] = self::getSignature($f_data,
+      $configuration['api_key']);
+
+    return $this->buildRedirectForm($form, $form_state, $redirect_url,
+      $f_data, $redirect_method);
+
+        
+  }
+
+  /**
+   * Signature generator
+   * @param      $data
+   * @param      $password
+   * @param bool $encoded
+   * @return string
+   *
+   */
+  public static function getSignature($data, $password, $encoded = TRUE)
+  {
+    $data = array_filter($data, function ($var) {
+      return $var !== '' && $var !== NULL;
+    });
+    ksort($data);
+    $str = $password;
+    foreach ($data as $v) {
+      $str .= self::SIGNATURE_SEPARATOR . $v;
+    }
+    if ($encoded) {
+      return sha1($str);
+    } else {
+      return $str;
+    }
+  }
+}
