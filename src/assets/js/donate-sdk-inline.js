6567d423957da7c62b4abf6fcf4963fe703aee0e
Roman K <karbivskij@gmail.com>
2022-04-24 23:23:04 +0300

    update js

--- src/assets/js/donate-sdk-inline.js
+++ src/assets/js/donate-sdk-inline.js
@@ -1,9 +1,11 @@
-PayPal.Donation.Button({
-    env:'production',
-    hosted_button_id:'L2PVSYTGQUECW',
-    image: {
-    src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif',
-    alt:'Donate with PayPal button',
-    title:'PayPal - The safer, easier way to pay online!',
-    }
-    }).render('#donate-button');
+(function(PayPal) {
+    PayPal.Donation.Button({
+        env:'production',
+        hosted_button_id:'L2PVSYTGQUECW',
+        image: {
+        src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif',
+        alt:'Donate with PayPal button',
+        title:'PayPal - The safer, easier way to pay online!',
+        }
+        }).render('#donate-button');
+    })(PayPal);
